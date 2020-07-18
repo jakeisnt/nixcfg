3789f43ffa0c690d943dda19f264c77a548d6013
Jacob Chvatal <jakechvatal@gmail.com>
2020-07-18 21:59:31 +0000

    added recommended xps hardware configuration

--- hosts/xps/default.nix
+++ hosts/xps/default.nix
@@ -1,12 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
+      <nixos-hardware/dell/xps/13-9370>
       ./hardware-configuration.nix
     ];
 
