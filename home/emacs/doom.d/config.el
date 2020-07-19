70b5965ca4b0941dc56f34d97ed9e6f1c03e2507
Jacob Chvatal <jakechvatal@gmail.com>
2020-07-19 05:24:41 +0000

    elfeed issues

--- home/emacs/doom.d/config.el
+++ home/emacs/doom.d/config.el
@@ -50,33 +50,33 @@
 
 
 ;; -------------------------------------------------------------------------- RSS
-(use-package! elfeed)
-(use-package! elfeed-protocol
-  :config
-  (elfeed-protocol-enable)
-  (setq elfeed-protocol-ttrss-maxsize 200
-        elfeed-set-timeout 36000
-        elfeed-show-entry-switch 'display-buffer
-        elfeed-search-remain-on-entry t
-        elfeed-feeds
-        '(("ttrss+https://jake@rss.chvatal.com"
-           :password (read-passwd "Provide your tt-rss password:")))))
+;; (use-package! elfeed) TODO
+;; (use-package! elfeed-protocol
+;;   :config
+;;   (elfeed-protocol-enable)
+;;   (setq elfeed-protocol-ttrss-maxsize 200
+;;         elfeed-set-timeout 36000
+;;         elfeed-show-entry-switch 'display-buffer
+;;         elfeed-search-remain-on-entry t
+;;         elfeed-feeds
+;;         '(("ttrss+https://jake@rss.chvatal.com"
+;;            :password (read-passwd "Provide your tt-rss password:")))))
 
-(after! elfeed
-  :init
-  ;; (map! :map elfeed-search-mode-map
-	;; 	 "n" (lambda () (interactive) (next-line) (call-interactively 'elfeed-search-show-entry))
-	;; 	 "p" (lambda () (interactive) (previous-line) (call-interactively 'elfeed-search-show-entry))
-	;; 	 "m" (lambda () (interactive) (apply 'elfeed-search-toggle-all '(star))))
-  (map!
-   :leader
-   :prefix "o"
-   :desc "elfeed" "e" #'elfeed)
-  (map! :leader
-        :prefix "e"
-        :desc "elfeed" "e" #'elfeed
-        :desc "elfeed-protocol-ttrss-update" "u" #'elfeed-protocol-ttrss-update
-        :desc "elfeed-protocol-ttrss-update-star" "s" #'elfeed-protocol-ttrss-update-star))
+;; (after! elfeed
+;;   :init
+;;   ;; (map! :map elfeed-search-mode-map
+;; 	;; 	 "n" (lambda () (interactive) (next-line) (call-interactively 'elfeed-search-show-entry))
+;; 	;; 	 "p" (lambda () (interactive) (previous-line) (call-interactively 'elfeed-search-show-entry))
+;; 	;; 	 "m" (lambda () (interactive) (apply 'elfeed-search-toggle-all '(star))))
+;;   (map!
+;;    :leader
+;;    :prefix "o"
+;;    :desc "elfeed" "e" #'elfeed)
+;;   (map! :leader
+;;         :prefix "e"
+;;         :desc "elfeed" "e" #'elfeed
+;;         :desc "elfeed-protocol-ttrss-update" "u" #'elfeed-protocol-ttrss-update
+;;         :desc "elfeed-protocol-ttrss-update-star" "s" #'elfeed-protocol-ttrss-update-star))
 
 
 ;; ------------------------------------------------------------------------ Org
@@ -86,7 +86,8 @@
       j/org-agenda-directory "~/org/agenda/"
       org-agenda-files (find-lisp-find-files j/org-agenda-directory "\.org$")
       org-default-notes-file "~/org/refile.org"
-      org-attach-id-dir "~/org/.attach/")
+      org-attach-id-dir "~/org/.attach/"
+      org-roam-directory "~/org/wiki/org/")
 
 
 (setq j/org-calendar-dir "~/org/calendar/")
@@ -94,6 +95,37 @@
 (setq org-log-done 'time
       org-log-into-drawer t
       org-log-state-notes-insert-after-drawers nil)
+
+
+(use-package! org
+  :mode ("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode)
+  :init
+  (map! :map org-mode-map
+        "M-n" #'outline-next-visible-heading
+        "M-p" #'outline-previous-visible-heading)
+  (setq org-src-window-setup 'current-window
+        org-return-follows-link t
+        org-babel-load-languages '((emacs-lisp . t)
+                                   (python . t)
+                                   (dot . t))
+        org-confirm-babel-evaluate nil
+        org-use-speed-commands t
+        org-catch-invisible-edits 'show org-preview-latex-image-directory "/tmp/ltximg/"
+        org-structure-template-alist '(("a" . "export ascii")
+                                       ("c" . "center")
+                                       ("C" . "comment")
+                                       ("e" . "example")
+                                       ("E" . "export")
+                                       ("h" . "export html")
+                                       ("l" . "export latex")
+                                       ("q" . "quote")
+                                       ("s" . "src")
+                                       ("v" . "verse")
+                                       ("el" . "src emacs-lisp")
+                                       ("d" . "definition")
+                                       ("t" . "theorem")))
+  (with-eval-after-load 'flycheck
+    (flycheck-add-mode 'proselint 'org-mode)))
 
 (use-package! org-chef
   :ensure t)
@@ -119,6 +151,121 @@
 (setq org-todo-keywords
       '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
         (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)")))
+
+(after! (org org-roam)
+  (use-package! org-roam-protocol)
+  (use-package! org-roam-server)
+  (setq org-roam-ref-capture-templates
+        '(("r" "ref" plain (function org-roam-capture--get-point)
+               "%?"
+               :file-name "websites/${slug}"
+               :head "#+TITLE: ${title}
+    #+ROAM_KEY: ${ref}
+    - source :: ${ref}"
+               :unnarrowed t)))
+  (defun j/org-roam-export-all ()
+    "Re-exports all Org-roam files to Hugo markdown."
+    (interactive)
+    (dolist (f (org-roam--list-all-files))
+      (with-current-buffer (find-file f)
+        (when (s-contains? "setupfile" (buffer-string))
+          (org-hugo-export-wim-to-md)))))
+  (defun j/org-roam--backlinks-list (file)
+    (when (org-roam--org-roam-file-p file)
+      (mapcar #'car (org-roam-db-query [:select :distinct [from]
+                                                :from links
+                                                :where (= to $s1)
+                                                :and from :not :like $s2] file "%private%"))))
+  (defun j/org-export-preprocessor (_backend)
+    (when-let ((links (j/org-roam--backlinks-list (buffer-file-name))))
+      (insert "\n** Backlinks\n")
+      (dolist (link links)
+        (insert (format "- [[file:%s][%s]]\n"
+                        (file-relative-name link org-roam-directory)
+                        (org-roam--get-title-or-slug link))))))
+
+  (defun j/org-roam-export-updated ()
+    "Re-export files that are linked to the current file."
+    (let ((files (org-roam-db-query [:select [to] :from links :where (= from $s1)] buffer-file-name)))
+      (interactive)
+      (dolist (f files)
+        (with-current-buffer (find-file-noselect (car f))
+          (when (s-contains? "setupfile" (buffer-string))
+            (org-hugo-export-wim-to-md))))))
+  (add-hook 'org-export-before-processing-hook #'j/org-export-preprocessor))
+
+(after! (org ox-hugo)
+  (defun j/conditional-hugo-enable ()
+    (save-excursion
+      (if (cdr (assoc "SETUPFILE" (org-roam--extract-global-props '("SETUPFILE"))))
+          (org-hugo-auto-export-mode +1)
+        (org-hugo-auto-export-mode -1))))
+  (add-hook 'org-mode-hook #'j/conditional-hugo-enable))
+
+(use-package! org-projectile
+  :init
+  (map! :leader
+        :prefix "p"
+        :desc "Add a TODO to a project" "n" #'org-projectile-project-todo-completing-read)
+  :config
+  (progn
+    (org-projectile-per-project)
+    (setq org-projectile-projects-file
+          "TODO.org")
+    (setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
+    (push (org-projectile-project-todo-entry) org-capture-templates))
+  :ensure t)
+
+
+(use-package! company-org-roam
+  :when (featurep! :completion company)
+  :after org-roam
+  :config
+  (set-company-backend! 'org-mode '(company-org-roam company-yasnippet company-dabbrev)))
+
+(use-package! deft ;; use deft to index org wiki files
+      :after org
+      :bind
+      ("C-c n d" . deft)
+      :custom
+      (deft-recursive t)
+      (deft-ignore-file-regexp "hugo_setup")
+      (deft-use-filename-as-title t)
+      (deft-use-filter-string-for-filename t)
+      (deft-default-extension "org")
+      (deft-directory "~/org/wiki/org/"))
+
+
+(use-package! org-journal ;; org-journal configuration
+  :config
+  (setq org-journal-date-prefix "#+TITLE: "
+        org-journal-file-format "%Y-%m-%d.org"
+        org-journal-dir "~/org/journal/"
+        org-journal-carryover-items nil)
+  (defun org-journal-today ()
+    (interactive)
+    (org-journal-new-entry t)))
+
+(use-package! bibtex-completion ;; autocompletion for notes templates
+  :config
+  (setq bibtex-completion-notes-path "~/org/wiki/org/"
+        bibtex-completion-bibliography "~/org/wiki/org/biblio.bib"
+        bibtex-completion-pdf-field "file"
+        bibtex-completion-notes-template-multiple-files
+        (concat
+         "#+title: ${title}\n"
+         "#+roam_key: cite:${=key=}\n"
+         "* TODO Notes\n"
+         ":PROPERTIES:\n"
+         ":Custom_ID: ${=key=}\n"
+         ":NOTER_DOCUMENT: %(orb-process-file-field \"${=key=}\")\n"
+         ":AUTHOR: ${author-abbrev}\n"
+         ":JOURNAL: ${journaltitle}\n"
+         ":DATE: ${date}\n"
+         ":YEAR: ${year}\n"
+         ":DOI: ${doi}\n"
+         ":URL: ${url}\n"
+         ":END:\n\n")))
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -201,3 +348,17 @@
        (let ((project-name (projectile-project-name)))
          (unless (string= "-" project-name)
            (format (if (buffer-modified-p)  " ◉ %s" "  ●  %s") project-name))))))
+
+
+(sp-local-pair ;; pair << and >>
+     '(org-mode)
+     "<<" ">>"
+     :actions '(insert))
+
+;; default browser is firefox
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "firefox")
+
+;; shared-clipboard
+(setq select-enable-clipboard t
+      interprogram-paste-function 'x-cut-buffer-or-selection-value)
