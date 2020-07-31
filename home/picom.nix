bf4649a65501aea773a2acdca4f51477756b2f0a
Jacob Chvatal <jakechvatal@gmail.com>
2020-07-31 02:48:41 -0700

    used more picom default options

--- home/picom.nix
+++ home/picom.nix
@@ -4,20 +4,16 @@
   services.picom = {
     enable = true;
     backend = "glx";
-    shadow = false;
+    vSync = true;
+    fade = true;
+    fadeDelta = 2;
+    shadow = true;
+    shadowExclude = [ ];
     extraOptions = ''
       glx-no-stencil = true;
       glx-no-rebind-pixmap = true;
       use-damage = true;
-      vsync = true;
       xrender-sync-fence = true;
-      shadow-exclude = [
-        "class_g = 'Polybar'",
-        "class_g = 'slop'",
-        "class_g = 'firefox' && argb",
-      ]
-      fading = true;
-      fade-delta = 2;
       mark-wmwin-focused = true;
       mark-ovredir-focused = true;
     '';
