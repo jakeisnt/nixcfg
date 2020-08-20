060bbe5ad57ada29e6d17d0bf3391530fc86087e
jakechv <jakechvatal@gmail.com>
2020-08-20 09:56:00 +0200

    added snippets package and todo

--- home/emacs/doom.d/init.el
+++ home/emacs/doom.d/init.el
@@ -66,7 +66,7 @@
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       ;; snippets          ; my elves. They type so I don't have to TODO
+       snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
