a4a896163f43dc5fe4c586397265e5808ea1b991
jakechv <jakechvatal@gmail.com>
2020-10-26 13:44:29 +0100

    Add newline

--- home/emacs/doom.d/modules/private/editor/packages.el
+++ home/emacs/doom.d/modules/private/editor/packages.el
@@ -24,5 +24,6 @@
 (package! editorconfig :pin "a6c70da2cf3a3a2aac2b204828f1acd243cd7529")
 (package! lsp-ui :pin "b1693d610c4d2c44305eba2719e8d4097fdcdcb8")
 (package! lsp-python-ms :pin "c4ebc7a11398733055a1dc07f9cffacd04d1c2dc")
+
 (provide 'packages)
 ;;; packages.el ends here
