0032b81460208efeb4ae3c3555eb615fdc9339ea
Jake Chvatal <jakechv@github.com>
2020-10-30 23:33:51 +0100

    Throw i3 into the mix

---
+++ modules/desktop/i3.nix
@@ -0,0 +1,74 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.i3;
+in {
+  options.modules.desktop.i3 = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    modules.theme.onReload.i3 = ''
+      ${pkgs.bspwm}/bin/bspc wm -r
+      source $XDG_CONFIG_HOME/bspwm/bspwmrc
+    '';
+
+    environment.systemPackages = with pkgs; [
+      lightdm
+      dunst
+      libnotify
+      (polybar.override {
+        pulseSupport = true;
+        nlSupport = true;
+        i3GapsSupport = true;
+      })
+    ];
+
+    services = {
+      picom.enable = true;
+      redshift.enable = true;
+      xserver = {
+        enable = true;
+        displayManager = {
+          lightdm.enable = true;
+          lightdm.greeters.mini.enable = true;
+        };
+        windowManager.i3 = {
+          enable = true;
+          package = pkgs.i3-caps;
+          config = rec {
+            
+            modifier = "Mod4";
+            focus.followMouse = false;
+            window.titlebar = false;
+            gaps = {
+              smartGaps = true;
+              outer = 0;
+              inner = 15;
+              top = 0;
+            };
+
+            startup = [{
+              command = "systemctl --user restart polybar";
+              always = true;
+              notification = false;
+            }];
+
+            keybindings = { "key combo" = "bound cmd"; };
+          }
+            
+        };
+      };
+    };
+
+    systemd.user.services."dunst" = {
+      enable = true;
+      description = "";
+      wantedBy = [ "default.target" ];
+      serviceConfig.Restart = "always";
+      serviceConfig.RestartSec = 2;
+      serviceConfig.ExecStart = "${pkgs.dunst}/bin/dunst";
+    };
+  };
+}
