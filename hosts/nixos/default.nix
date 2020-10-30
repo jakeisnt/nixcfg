33d13bac49aa5f28893a2b94d80e68a82bedc997
Jake Chvatal <jakechv@github.com>
2020-10-30 17:29:18 +0000

    asdf

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -101,5 +101,73 @@
     experimental-features = nix-command flakes
 '';
 
+
+  ## Modules
+  modules = {
+    desktop = {
+      bspwm.enable = true;
+      apps = {
+        discord.enable = true;
+        rofi.enable = true;
+        # skype.enable = true;
+      };
+      browsers = {
+        default = "firefox";
+        firefox.enable = true;
+      };
+      media = {
+        # daw.enable = true;
+        documents.enable = true;
+        graphics.enable = true;
+        mpv.enable = true;
+        recording.enable = true;
+        spotify.enable = true;
+      };
+      term = {
+        default = "xst";
+        st.enable = true;
+      };
+      vm = {
+        # virtualbox.enable = true;
+      };
+    };
+    editors = {
+      default = "nvim";
+      emacs.enable = true;
+      vim.enable = true;
+    };
+    dev = {
+      # cc.enable = true;
+      # common-lisp.enable = true;
+      # rust.enable = true;
+      # lua.enable = true;
+      # lua.love2d.enable = true;
+    };
+    hardware = {
+      audio.enable = true;
+      fs = {
+        enable = true;
+        ssd.enable = true;
+      };
+    };
+    shell = {
+      direnv.enable = true;
+      git.enable = true;
+      gnupg.enable = true;
+      # weechat.enable = true;
+      pass.enable = true;
+      tmux.enable = true;
+      # ranger.enable = true;
+      zsh.enable = true;
+    };
+    hardware = {
+
+    };
+    services = {
+      # syncthing.enable = true;
+      ssh.enable = true;
+    };
+    theme.active = "alucard";
+  };
 }
 
