5eaa12377f7da6182376780925d3384061c096a3
Jake Chvatal <jakechv@github.com>
2020-11-02 11:59:08 +0100

    Add pulseaudio configuration fixes

--- modules/hardware/audio.nix
+++ modules/hardware/audio.nix
@@ -10,21 +10,29 @@ in {
 
   config = mkIf cfg.enable {
     sound.enable = true;
-    hardware.pulseaudio.enable = true;
+    hardware.pulseaudio = {
+      enable = true;
+      daemon.config = { default-sample-rate = 48000; };
+      support32Bit = true;
 
-    # HACK Prevents ~/.esd_auth files by disabling the esound protocol module
-    #      for pulseaudio, which I likely don't need. Is there a better way?
-    hardware.pulseaudio.configFile =
-      let inherit (pkgs) runCommand pulseaudio;
-          paConfigFile =
-            runCommand "disablePulseaudioEsoundModule"
-              { buildInputs = [ pulseaudio ]; } ''
+      extraConfig = ''
+      load-module module-switch-on-connect
+    '';
+
+      # HACK Prevents ~/.esd_auth files by disabling the esound protocol module
+      #      for pulseaudio, which I likely don't need. Is there a better way?
+      configFile =
+        let inherit (pkgs) runCommand pulseaudio;
+            paConfigFile =
+              runCommand "disablePulseaudioEsoundModule"
+                { buildInputs = [ pulseaudio ]; } ''
                 mkdir "$out"
                 cp ${pulseaudio}/etc/pulse/default.pa "$out/default.pa"
                 sed -i -e 's|load-module module-esound-protocol-unix|# ...|' "$out/default.pa"
               '';
-      in mkIf config.hardware.pulseaudio.enable
-        "${paConfigFile}/default.pa";
+        in mkIf config.hardware.pulseaudio.enable
+          "${paConfigFile}/default.pa";
+    };
 
     user.extraGroups = [ "audio" ];
   };
