f11e6e831ac6da3ae4fd39e12a4de8127ef40491
jakeisnt <jakechv@github.com>
2020-11-15 18:06:13 +0100

    Configure SDK environment variables (not sure if this works)

--- modules/dev/android.nix
+++ modules/dev/android.nix
@@ -3,6 +3,8 @@
 with lib;
 with lib.my;
 let cfg = config.modules.dev.android;
+    androidSdk = pkgs.androidenv.androidPkgs_9_0.androidsdk;
+    platformTools = pkgs.androidenv.androidPkgs_9_0.platform-tools;
 in {
   options.modules.dev.android = {
     enable = mkBoolOpt false;
@@ -11,13 +13,24 @@ in {
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
       android-studio
-      androidenv.androidPkgs_9_0.androidsdk
+      androidSdk
+      platformTools
       glibc
+      openjdk8
     ];
     programs.adb.enable = true;
     user.extraGroups = ["adbusers"];
     services.udev.packages = [
       pkgs.android-udev-rules
     ];
+    environment.variables.ANDROID_JAVA_HOME="${pkgs.openjdk8.home}";
+    environment.variables.ANDROID_SDK_ROOT="${androidSdk}";
+    environment.variables.GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/28.0.3/aapt2";
+
+    env.ANDROID_JAVA_HOME="${pkgs.openjdk8.home}";
+    env.ANDROID_SDK_ROOT="${androidSdk}";
+
+    # override the aapt2 that gradle uses with the nix-shipped version
+    env.GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/28.0.3/aapt2";
   };
 }
