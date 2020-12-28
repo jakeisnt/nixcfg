6eb08d794598663592061a3eadc9e7c909e4539d
jakeisnt <jakechv@github.com>
2020-12-28 14:49:17 -0800

    Fix Netsurf

--- modules/desktop/browsers/netsurf.nix
+++ modules/desktop/browsers/netsurf.nix
@@ -1,9 +1,10 @@
 { options, config, lib, pkgs, ... }:
 
+# a JavaScript-free web browser.
 with lib;
 with lib.my;
 let cfg = config.modules.desktop.browsers.netsurf;
 in {
   options.modules.desktop.browsers.netsurf = { enable = mkBoolOpt false; };
-  config = mkIf cfg.enable { user.packages = with pkgs; [ netsurf ]; };
+  config = mkIf cfg.enable { user.packages = with pkgs; [ netsurf-browser ]; };
 }
