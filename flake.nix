343a6c60089a6fb84b430295d347105a6f1391c4
jakeisnt <jakechv@github.com>
2020-12-28 22:39:11 -0800

    Don't export 'lib'

--- flake.nix
+++ flake.nix
@@ -10,62 +10,59 @@
 {
   description = "A grossly incandescent nixos config.";
 
-  inputs = 
-    {
-      # Core dependencies.
-      # Two inputs so I can track them separately at different rates.
-      nixpkgs.url          = "nixpkgs/master";
-      nixpkgs-unstable.url = "nixpkgs/master";
+  inputs = {
+    # Core dependencies.
+    # Two inputs so I can track them separately at different rates.
+    nixpkgs.url = "nixpkgs/master";
+    nixpkgs-unstable.url = "nixpkgs/master";
 
-      home-manager.url   = "github:rycee/home-manager/master";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:rycee/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-      # Extras
-      emacs-overlay.url  = "github:nix-community/emacs-overlay";
-      nixos-hardware.url = "github:nixos/nixos-hardware";
-    };
+    # Extras
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+  };
 
-  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
       inherit (lib) attrValues;
       inherit (lib.my) mapModules mapModulesRec mapHosts;
 
       system = "x86_64-linux";
 
-      mkPkgs = pkgs: extraOverlays: import pkgs {
-        inherit system;
-        config = {
-          android_sdk.accept_license = true;
-          allowUnfree = true;  # forgive me Stallman senpai
+      mkPkgs = pkgs: extraOverlays:
+        import pkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true; # forgive me Stallman senpai
+          };
+          overlays = extraOverlays ++ (attrValues self.overlays);
         };
-        overlays = extraOverlays ++ (attrValues self.overlays);
-      };
-      pkgs  = mkPkgs nixpkgs [ self.overlay ];
-      uPkgs = mkPkgs nixpkgs-unstable [];
+      pkgs = mkPkgs nixpkgs [ self.overlay ];
+      uPkgs = mkPkgs nixpkgs-unstable [ ];
 
-      lib = nixpkgs.lib.extend
-        (self: super: { my = import ./lib { inherit pkgs inputs; lib = self; }; });
+      lib = nixpkgs.lib.extend (self: super: {
+        my = import ./lib {
+          inherit pkgs inputs;
+          lib = self;
+        };
+      });
     in {
-      lib = lib.my;
+      overlay = final: prev: {
+        unstable = uPkgs;
+        my = self.packages."${system}";
+      };
 
-      overlay =
-        final: prev: {
-          unstable = uPkgs;
-          my = self.packages."${system}";
-        };
+      overlays = mapModules ./overlays import;
 
-      overlays =
-        mapModules ./overlays import;
+      packages."${system}" = mapModules ./packages (p: pkgs.callPackage p { });
 
-      packages."${system}" =
-        mapModules ./packages
-          (p: pkgs.callPackage p {});
+      nixosModules = {
+        dotfiles = import ./.;
+      } // mapModulesRec ./modules import;
 
-      nixosModules =
-        { dotfiles = import ./.; }
-        // mapModulesRec ./modules import;
-
-      nixosConfigurations =
-        mapHosts ./hosts { inherit system; };
+      nixosConfigurations = mapHosts ./hosts { inherit system; };
     };
 }
