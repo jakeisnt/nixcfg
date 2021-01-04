4118097ee476e407deb3e42c4496c24bfacb0cf5
jakeisnt <jakechv@github.com>
2021-01-04 13:29:15 -0800

    Add notes for a better GTK RSS reader

--- modules/messengers/rss.nix
+++ modules/messengers/rss.nix
@@ -6,10 +6,10 @@ let cfg = config.modules.messengers.rss;
 in {
   options.modules.messengers.rss = { enable = mkBoolOpt false; };
   config = mkIf cfg.enable {
-    # feedreader: nice gui for RSS, if newsboat becomes unwieldy
+    # 'liferea' provides a nice GUI for RSS if I need images and html at some point
     user.packages = with pkgs; [ newsboat ];
-    # we want to be able to tweak .config/newsboat/urls without rebuild, so it's not included here.
     # TODO: consider tt-rss server (or mimic the protocol with another service?)
+    # we want to be able to tweak .config/newsboat/urls without rebuild, so it's not included here.
     home.configFile = {
       "newsboat/config".source = "${configDir}/newsboat/config";
     };
