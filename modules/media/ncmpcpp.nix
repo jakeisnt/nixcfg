e89011596abdbe95261ab1a9e7859438aec4cf74
jakeisnt <jakechv@github.com>
2021-01-11 17:12:36 -0800

    Inline hidden config files to show secrets

--- modules/media/ncmpcpp.nix
+++ modules/media/ncmpcpp.nix
@@ -64,7 +64,100 @@ in {
     home.configFile = {
       "ncmpcpp/config".source = "${configDir}/ncmpcpp/config";
       "ncmpcpp/bindings".source = "${configDir}/ncmpcpp/bindings";
-      "mopidy/mopidy.conf".source = "${secretsDir}/mopidy/mopidy.conf";
+      "mopidy/mopidy.conf".text = ''
+        [core]
+        cache_dir = $XDG_CACHE_DIR/mopidy
+        config_dir = $XDG_CONFIG_DIR/mopidy
+        data_dir = $XDG_DATA_DIR/mopidy
+        max_tracklist_length = 10000
+        restore_state = false
+
+        [logging]
+        verbosity = 0
+        format = %(levelname)-8s %(asctime)s [%(process)d:%(threadName)s] %(name)s
+          %(message)s
+        color = true
+
+        [audio]
+        mixer = software
+        # output = pulsesink server=127.0.0.1
+        output = autoaudiosink
+
+        [proxy]
+        #scheme =
+        #hostname =
+        # port = 6600
+        #username =
+        #password =
+
+        [file]
+        enabled = true
+        media_dirs =
+          $HOME/music
+        excluded_file_extensions =
+          .directory
+          .html
+          .jpeg
+          .jpg
+          .log
+          .nfo
+          .pdf
+          .png
+          .txt
+          .zip
+          .git
+          .org
+        show_dotfiles = false
+        follow_symlinks = false
+
+        [http]
+        enabled = true
+        hostname = 127.0.0.1
+        port = 6680
+
+        [m3u]
+        enabled = true
+
+        [softwaremixer]
+        # enabled = false
+
+        [stream]
+        enabled = true
+        protocols =
+          http
+          https
+          mms
+          rtmp
+          rtmps
+          rtsp
+        #metadata_blacklist =
+        #timeout = 5000
+
+        [youtube]
+        enabled = true
+        autoplay_enabled = true
+
+        [mpd]
+        enabled = true
+
+        [spotify]
+        allow_network = true
+        search_album_count = 20
+        search_artist_count = 10
+        search_track_count = 50
+        enabled = true
+        allow_playlists = true
+        allow_cache = true
+
+        username = ${secrets.spotify.username}
+        password = ${secrets.spotify.password}
+        client_id = ${secrets.spotify.clientID}
+        client_secret = ${secrets.spotify.clientSecret}
+
+        [scrobbler]
+        username = ${secrets.lastFM.username}
+        password = ${secrets.lastFM.password}
+      '';
     };
   };
 }
--- modules/messengers/email.nix
+++ modules/messengers/email.nix
@@ -21,9 +21,138 @@ in {
     };
 
     home.configFile = {
-      "offlineimap" = { source = "${secretsDir}/offlineimap"; };
-      "mutt" = { source = "${secretsDir}/mutt"; };
-      "msmtp" = { source = "${secretsDir}/msmtp"; };
+      "offlineimap/config".text = ''
+        [general]
+        # List of accounts to be synced, separated by a comma.
+        ui = ttyui
+        # gmail could also be an account
+        accounts = isnt
+
+        # [Account gmail]
+        # # Identifier for the local repository; e.g. the maildir to be synced via IMAP.
+        # localrepository = gmail-local
+        # # Identifier for the remote repository; i.e. the actual IMAP, usually non-local.
+        # remoterepository = gmail-remote
+
+        # [Repository gmail-local]
+        # # OfflineIMAP supports Maildir, GmailMaildir, and IMAP for local repositories.
+        # type = Maildir
+        # # Where should the mail be placed?
+        # localfolders = ~/.mail/gmail
+
+        # [Repository gmail-remote]
+        # # Remote repos can be IMAP or Gmail, the latter being a preconfigured IMAP.
+        # type = Gmail
+        # remoteuser = ${secrets.gmail.user}
+        # remotepass = ${secrets.gmail.password}
+        # realdelete = no
+        # maxconnections = 3
+        # sslcacertfile = /etc/ssl/certs/ca-certificates.crt
+
+        [Account isnt]
+        localrepository = isnt-local
+        remoterepository = isnt-remote
+
+        [Repository isnt-local]
+        type = Maildir
+        ssl=yes
+        localfolders = ~/.mail/isnt
+
+        [Repository isnt-remote]
+        type = IMAP
+        remotehost = ${secrets.email.host}
+        remoteuser = ${secrets.email.user}
+        remotepass = ${secrets.email.password}
+        realdelete = no
+        maxconnections = 3
+        sslcacertfile = /etc/ssl/certs/ca-certificates.crt
+      '';
+      "mutt/muttrc".text = ''
+        set mbox_type   = Maildir
+        set sendmail    = "/usr/bin/env msmtp"
+
+        set folder      = ~/.mail/isnt
+        set spoolfile   = "+INBOX"
+        set mbox        = "+[isnt]/All Mail"
+        set postponed   = "+[isnt]/Drafts"
+        unset record
+
+        mailboxes +INBOX
+
+        macro index D \
+            "<save-message>+[Gmail]/Trash<enter>" \
+            "move message to the trash"
+
+        macro index S \
+            "<save-message>+[Gmail]/Spam<enter>" \
+            "mark message as spam"
+
+        # main options
+        set realname   = "${secrets.name}"
+        set from       = "${secrets.email.user}"
+        set mail_check = 0
+        set envelope_from
+
+        unset move           # gmail does that
+        set delete           # don't ask, just do
+        unset confirmappend  # don't ask, just do!
+        set quit             # don't ask, just do!!
+        unset mark_old       # read/new is good enough for me
+
+        # sort/threading
+        set sort     = threads
+        set sort_aux = reverse-last-date-received
+        set sort_re
+
+        # look and feel
+        set reverse_name = yes
+        set reverse_realname = yes
+
+        set pager_index_lines = 8
+        set pager_context     = 5
+        set pager_stop
+        set menu_scroll
+        set smart_wrap
+        set tilde
+        unset markers
+
+        # Status Bar
+        set status_chars  = " *%A"
+        set status_format = "───[ Folder: %f ]───[%r%m messages%?n? (%n new)?%?d? (%d to delete)?%?t? (%t tagged)? ]───%>─%?p?( %p postponed )?───"
+
+        # Other binds to make mutt behave more like Vim, optional.
+        bind pager j next-line
+        bind pager k previous-line
+        bind attach,index,pager \CD next-page
+        bind attach,index,pager \CU previous-page
+        bind pager g top
+        bind pager G bottom
+        bind attach,index g first-entry
+        bind attach,index G last-entry
+
+        # composing
+        set fcc_attach
+        unset mime_forward
+        set forward_format = "Fwd: %s"
+        set include
+        set forward_quote
+
+        ignore *                               # first, ignore all headers
+        unignore from: to: cc: date: subject:  # then, show only these
+        hdr_order from: to: cc: date: subject: # and in this order
+      '';
+      "msmtp/config".text = ''
+        account default
+        host ${secrets.email.host}
+        port 587
+        protocol smtp
+        auth on
+        from ${secrets.email.user}
+        user ${secrets.email.user}
+        password ${secrets.email.password}
+        tls on
+        tls_nocertcheck
+      '';
     };
   };
 }
