929d748d2c287891f8ede680ae7750546f7eafa8
jakeisnt <jakechv@github.com>
2021-01-19 19:32:51 -0800

    Fix android studio not showing up

--- modules/dev/android.nix
+++ modules/dev/android.nix
@@ -2,13 +2,12 @@
 # https://nixos.wiki/wiki/Android
 with lib;
 with lib.my;
-let cfg = config.modules.dev.android;
-    androidSdk = pkgs.androidenv.androidPkgs_9_0.androidsdk;
-    platformTools = pkgs.androidenv.androidPkgs_9_0.platform-tools;
+let
+  cfg = config.modules.dev.android;
+  androidSdk = pkgs.androidenv.androidPkgs_9_0.androidsdk;
+  platformTools = pkgs.androidenv.androidPkgs_9_0.platform-tools;
 in {
-  options.modules.dev.android = {
-    enable = mkBoolOpt false;
-  };
+  options.modules.dev.android = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
@@ -19,18 +18,20 @@ in {
       openjdk8
     ];
     programs.adb.enable = true;
-    user.extraGroups = ["adbusers"];
-    services.udev.packages = [
-      pkgs.android-udev-rules
-    ];
-    environment.variables.ANDROID_JAVA_HOME="${pkgs.openjdk8.home}";
-    environment.variables.ANDROID_SDK_ROOT="${androidSdk}";
-    environment.variables.GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/28.0.3/aapt2";
+    user.extraGroups = [ "adbusers" ];
+    services.udev.packages = [ pkgs.android-udev-rules ];
+    environment.variables.ANDROID_JAVA_HOME = "${pkgs.openjdk8.home}";
+    environment.variables.ANDROID_SDK_ROOT = "${androidSdk}";
+    environment.variables.GRADLE_OPTS =
+      "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/28.0.3/aapt2";
+    # https://github.com/swaywm/sway/issues/595
+    environment.variables._JAVA_AWT_WM_NONREPARENTING = 1;
 
-    env.ANDROID_JAVA_HOME="${pkgs.openjdk8.home}";
-    env.ANDROID_SDK_ROOT="${androidSdk}";
+    env.ANDROID_JAVA_HOME = "${pkgs.openjdk8.home}";
+    env.ANDROID_SDK_ROOT = "${androidSdk}";
 
     # override the aapt2 that gradle uses with the nix-shipped version
-    env.GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/28.0.3/aapt2";
+    env.GRADLE_OPTS =
+      "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/28.0.3/aapt2";
   };
 }
