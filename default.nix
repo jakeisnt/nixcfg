a201d052af71823c173590a353fd21f7c61d7c0a
jakeisnt <jakechv@github.com>
2021-01-22 18:06:39 -0800

    remove nix-community

--- default.nix
+++ default.nix
@@ -31,11 +31,9 @@ with inputs; {
     ];
     binaryCaches = [
       "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
       "https://nix-cfg.cachix.org"
     ];
     binaryCachePublicKeys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-cfg.cachix.org-1:K5log6+7FTtbqKDRwAhcJvFEdOiavE0aIUWBsYfns4Y="
     ];
     registry = {
