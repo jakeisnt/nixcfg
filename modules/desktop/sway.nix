9d128cb798a3f8000e57a8eb2546d335fdbac78e
Jacob Chvatal <jake@isnt.online>
2021-02-02 21:07:47 -0800

    Add ly, but not replacing the tty

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -9,8 +9,43 @@ in {
   options.modules.desktop.sway = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
+    hardware.opengl.enable = true;
     env.XDG_SESSION_TYPE = "wayland";
     modules.desktop.apps.rofi.enable = true;
+
+    # from https://github.com/nullgemm/ly/blob/master/res/ly.service
+    systemd.services.ly= {
+      description = "TUI display manager";
+      after = ["systemd-user-sessions.service" "plymouth-quit-wait.service" "getty@tty1.service"];
+      wantedBy = ["multi-user.target"];
+      aliases = ["display-manager.service"];
+      serviceConfig = {
+        Type = "idle";
+        ExecStart = "${pkgs.ly}/bin/ly";
+        StandardInput= "tty";
+        TTYPath="/dev/tty1";
+        TTYReset="yes";
+        TTYVHangup="yes";
+      };
+    };
+
+    #   [Unit]
+    #   After=
+    #
+    #   [Service]
+    #   Type=idle
+    #   ExecStart=/usr/bin/ly
+    #
+    # '';
+
+    # services.xserver.desktopManager.session = singleton
+    # { name = "ly";
+    # start = ''
+    #   ${pkgs.ly}/bin/ly &
+    #     waitPID=$!
+    #   '';
+    # };
+
     programs.sway = {
       enable = true;
       extraPackages = with pkgs; [
@@ -35,86 +70,87 @@ in {
 
     home.configFile = {
       "sway/config".text = with colors;
-        concatStrings [
-          (''
-            set $foreground #${foreground}
-            set $background #${background}
-            set $lighterbg  #${fadeColor}
-            set $urgent #${urgent}
-            set $urgenttext #F8F8F2
-            set $inactiveback #44475A
-            set $pholdback #282A36
-            set $focusedback #6f757d
-          '')
+      concatStrings [
+        (''
+        set $foreground #${foreground}
+        set $background #${background}
+        set $lighterbg  #${fadeColor}
+        set $urgent #${urgent}
+        set $urgenttext #F8F8F2
+        set $inactiveback #44475A
+        set $pholdback #282A36
+        set $focusedback #6f757d
+        '')
           (concatMapStringsSep "\n" readFile [ "${configDir}/sway/config" ])
-        ];
-      "mako/config".text = with colors; ''
-        sort=-time
-        layer=overlay
-        max-visible=-1
-        background-color=#${background}
-        border-color=#${color0}
-        text-color=#${foreground}
-        width=300
-        height=110
-        border-size=1
-        default-timeout=5000
-        ignore-timeout=1
-        margin=10,12
+          ];
+          "mako/config".text = with colors; ''
+          sort=-time
+          layer=overlay
+          max-visible=-1
+          background-color=#${background}
+          border-color=#${color0}
+          text-color=#${foreground}
+          width=300
+          height=110
+          border-size=1
+          default-timeout=5000
+          ignore-timeout=1
+          margin=10,12
 
-        [urgency=low]
-        background-color=#${background}
-        border-color=#${color0}
+          [urgency=low]
+          background-color=#${background}
+          border-color=#${color0}
 
-        [urgency=normal]
-        background-color=#${background}
-        border-color=#${color0}
+          [urgency=normal]
+          background-color=#${background}
+          border-color=#${color0}
 
-        [urgency=high]
-        background-color=#${urgent}
-        border-color=#${urgent}
-        default-timeout=0
+          [urgency=high]
+          background-color=#${urgent}
+          border-color=#${urgent}
+          default-timeout=0
 
-        [category=mpd]
-        default-timeout=2000
-        group-by=category
+          [category=mpd]
+          default-timeout=2000
+          group-by=category
 
-        [category=spotify]
-        default-timeout=2000
-        group-by=category
-      '';
-    };
+          [category=spotify]
+          default-timeout=2000
+          group-by=category
+          '';
+        };
 
-    environment.systemPackages = with pkgs;
-      [
-        (pkgs.writeTextFile {
-          name = "startsway";
-          destination = "/bin/startsway";
-          executable = true;
-          text = ''
+        environment.systemPackages = with pkgs;
+        [
+          (pkgs.writeTextFile {
+            name = "startsway";
+            destination = "/bin/startsway";
+            executable = true;
+            text = ''
             #! ${pkgs.bash}/bin/bash
 
             # first import environment variables from the login manager
-            systemctl --user import-environment
+              systemctl --user import-environment
             # then start the service
-            exec systemctl --user start sway.service
-          '';
-        })
-      ];
-    systemd.user.targets.sway-session = {
-      description = "Sway compositor session";
-      documentation = [ "man:systemd.special(7)" ];
-      bindsTo = [ "graphical-session.target" ];
-      wants = [ "graphical-session-pre.target" ];
-      after = [ "graphical-session-pre.target" ];
-    };
+              exec systemctl --user start sway.service
+            '';
+          })
+        ];
 
-    systemd.user.services.sway = {
-      description = "Sway - Wayland window manager";
-      documentation = [ "man:sway(5)" ];
-      bindsTo = [ "graphical-session.target" ];
-      wants = [ "graphical-session-pre.target" ];
-      after = [ "graphical-session-pre.target" ];
+        systemd.user.targets.sway-session = {
+          description = "Sway compositor session";
+          documentation = [ "man:systemd.special(7)" ];
+          bindsTo = [ "graphical-session.target" ];
+          wants = [ "graphical-session-pre.target" ];
+          after = [ "graphical-session-pre.target" ];
+        };
+
+        systemd.user.services.sway = {
+          description = "Sway - Wayland window manager";
+          documentation = [ "man:sway(5)" ];
+          bindsTo = [ "graphical-session.target" ];
+          wants = [ "graphical-session-pre.target" ];
+          after = [ "graphical-session-pre.target" ];
       # We explicitly unset PATH here, as we want it to be set by
       # systemctl --user import-environment in startsway
       environment.PATH = lib.mkForce null;
@@ -131,7 +167,6 @@ in {
 
     services.redshift = {
       enable = true;
-      # package = pkgs.redshift-wlr;
       extraOptions = [ "-m" "wayland" ];
     };
 
