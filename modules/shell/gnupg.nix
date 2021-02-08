a8c9857e05eaa29fcd2deb8e439b7b14d87033ea
Jacob Chvatal <jake@isnt.online>
2021-02-08 12:04:04 -0800

    attempt to fix gnupg

--- modules/shell/gnupg.nix
+++ modules/shell/gnupg.nix
@@ -13,16 +13,19 @@ in {
     environment.variables.GNUPGHOME = "$XDG_CONFIG_HOME/gnupg";
 
     programs.gnupg.agent.enable = true;
-    user.packages = with pkgs; [ tomb pinentry.curses ];
+    user.packages = with pkgs; [ tomb pinentry.curses pinentry-gnome ];
 
     # HACK Without this config file you get "No pinentry program" on 20.03.
     #      programs.gnupg.agent.pinentryFlavor doesn't appear to work, and this
     #      is cleaner than overriding the systemd unit.
+    # pinentry-program ${pkgs.pinentry.curses}/bin/pinentry
     home.configFile."gnupg/gpg-agent.conf" = {
       text = ''
+        allow-emacs-pinentry
+        allow-loopback-pinentry
         default-cache-ttl ${toString cfg.cacheTTL}
-        pinentry-program ${pkgs.pinentry.curses}/bin/pinentry
       '';
     };
   };
 }
+
