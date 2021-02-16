fae91b67b4545d0925b6cf445cb2dbb554a2bd76
Jacob Chvatal <jake@isnt.online>
2021-02-16 14:06:26 -0800

    Enhance chromium module

--- modules/desktop/browsers/chrome.nix
+++ modules/desktop/browsers/chrome.nix
@@ -6,6 +6,38 @@ let cfg = config.modules.desktop.browsers.chrome;
 in {
   options.modules.desktop.browsers.chrome = { enable = mkBoolOpt false; };
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [ ungoogled-chromium google-chrome ];
+    programs.chromium = {
+      enable = true;
+      # TODO: change to something more secure than google
+      defaultSearchProviderSearchURL =
+        "https://encrypted.google.com/search?q={searchTerms}&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:searchClient}{google:sourceId}{google:instantExtendedEnabledParameter}ie={inputEncoding}";
+      defaultSearchProviderSuggestURL =
+        "https://encrypted.google.com/complete/search?output=chrome&q={searchTerms}";
+      homepageLocation = "https://github.com";
+      extensions = [
+        "nngceckbapebfimnlniiiahkandclblb" # bitwarden
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
+        "kkkjlfejijcjgjllecmnejhogpbcigdc" # org-capture
+        "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
+      ];
+
+      # add more options when using chromium !!
+      # info found here: https://cloud.google.com/docs/chrome-enterprise/policies/#miscellaneous
+      extraOpts = {
+        "BrowserSignin" = 0;
+        "PasswordManagerEnabled" = false;
+        "SyncDisabled" = true;
+        "CloudReportingEnabled" = false;
+        "SafeBrowsingEnabled" = false;
+        "ReportSafeBrowsingData" = false;
+        "AllowDinosaurEasterEgg" = false; # : (
+        "AllowOutdatedPlugins" = true;
+
+        # voice assistant
+        "VoiceInteractionContextEnabled" = false;
+        "VoiceInteractionHotwordEnabled" = false;
+        "VoiceInteractionQuickAnswersEnabled" = false;
+      };
+    };
   };
 }
