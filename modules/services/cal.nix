2948b0c6b86efab2f8eb85f130892cd3ce6bf209
Jacob Chvatal <jake@isnt.online>
2021-06-21 17:37:19 -0700

    dont use silly custom server things?

--- modules/services/cal.nix
+++ modules/services/cal.nix
@@ -18,7 +18,7 @@ in {
     services.radicale = {
       enable = true;
       settings = {
-        server = { hosts = [ "0.0.0.0:5232" "127.0.0.1:5232" "[::]:5232" ]; };
+        # server = { hosts = [ "0.0.0.0:5232" "127.0.0.1:5232" "[::]:5232" ]; };
         auth = {
           type = "htpasswd";
           htpasswd_filename = "${htpasswd}";
