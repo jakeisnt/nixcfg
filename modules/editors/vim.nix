0a144db704ed54f349bdc91915a9e32f41b1f5b2
Jacob Chvatal <jake@isnt.online>
2021-06-24 16:56:13 -0700

    working michael config

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -1,4 +1,4 @@
-{ config, options, lib, pkgs, ... }:
+{ config, options, lib, pkgs, inputs, ... }:
 
 # If I ever want to improve my Vim configuration,
 # https://github.com/gvolpe/nix-config/tree/master/home/programs/neovim
@@ -11,12 +11,65 @@ in {
   options.modules.editors.vim = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [ editorconfig-core-c neovim ];
+    nixpkgs.overlays = with inputs; [ neovim-nightly-overlay.overlay ];
+    user.packages = with pkgs; [ editorconfig-core-c ];
+
+    programs.neovim = {
+      package = pkgs.neovim-nightly;
+      enable = true;
+      defaultEditor = false; # this is configured by me elsewhere
+      # viAlias = true;
+      # vimAlias = true;
+
+      configure = {
+        customRC = ''
+          lua require('init')
+        '';
+
+        packages.myPlugins = with pkgs.vimPlugins; {
+
+          start = [
+            vim-polyglot
+            vim-commentary
+            nvim-treesitter
+            nvim-lspconfig
+            completion-nvim
+            telescope-nvim
+            popup-nvim
+            plenary-nvim
+            onedark-vim
+            lualine-nvim
+            which-key-nvim
+            neogit
+            vim-gitgutter
+            auto-pairs
+          ];
+        };
+      };
+
+      # plugins = with pkgs.vimPlugins; [
+      #   vim-polyglot
+      #   vim-commentary
+      #   nvim-treesitter
+      #   nvim-lspconfig
+      #   completion-nvim
+      #   telescope-nvim
+      #   popup-nvim
+      #   plenary-nvim
+      #   onedark-vim
+      #   lualine-nvim
+      #   which-key-nvim
+      #   neogit
+      #   vim-gitgutter
+      #   auto-pairs
+      # ];
+    };
 
     env = {
       VIMINIT =
         "let \\$MYVIMRC='\\$XDG_CONFIG_HOME/nvim/init.vim' | source \\$MYVIMRC";
     };
+
     environment.shellAliases = {
       vim = "nvim";
       vi = "nvim";
