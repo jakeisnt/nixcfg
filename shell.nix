3dfa6cb70900a795fc0275c92033d8e4bcd4b748
Jacob Chvatal <jake@isnt.online>
2021-09-26 19:43:34 -0400

    give flake path back to shell

--- shell.nix
+++ shell.nix
@@ -2,27 +2,17 @@
 # https://github.com/hlissner/dotfiles/blob/master/shell.nix
 with pkgs;
 let
-  # nixBin = writeShellScriptBin "nix" ''
-  #   ${nixFlakes}/bin/nix --option experimental-features "nix-command flakes" "$@"
-  #   '';
-  # sops-nix = builtins.fetchTarball {
-  #   url = "https://github.com/Mic92/sops-nix/archive/master.tar.gz";
-  #   sha256 = "0ga528x3sci1y10vd8m5l2wnsl7kkf14whwp7dkwcbkvm34n88yx";
-  # };
   sops-bins = pkgs.callPackage sops-nix {};
 in
 mkShell {
   sopsPGPKeyDirs = [ ./keys ];
-  # sopsCreateGPGHome = true;
   buildInputs = [ git nix-zsh-completions rnix-lsp nixpkgs-fmt ];
-
   nativeBuildInputs = with sops-bins; [
     sops
     sops-import-keys-hook
     sops-init-gpg-key
   ];
-  # shellHook = ''
-  #   export FLAKE="$(pwd)"
-  #   export PATH="$FLAKE/bin:${nixBin}/bin:$PATH"
-  # '';
+  shellHook = ''
+    export FLAKE="$(pwd)"
+  '';
 }
