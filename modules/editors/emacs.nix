d266433251b6e6335b513a5abd5ba2112366a52f
Jacob Chvatal <jake@isnt.online>
2021-09-29 16:26:28 -0400

    autoinstall my doom config if it's not around

--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -25,12 +25,16 @@ let
       mkdir -p $HOME/.emacs.d
       git -C $HOME/.emacs.d init
     fi
+    if [ ! -d $HOME/.config/doom ]; then
+      git clone git@github.com:jakeisnt/doom.d.git $HOME/.config/doom
+    fi
     if [ $(git -C $HOME/.emacs.d rev-parse HEAD) != ${pkgs.doomEmacsRevision} ]; then
       git -C $HOME/.emacs.d fetch https://github.com/hlissner/doom-emacs.git || true
       git -C $HOME/.emacs.d checkout ${pkgs.doomEmacsRevision} || true
       $HOME/.emacs.d/bin/doom sync || true
       YES=1 FORCE=1 $HOME/.emacs.d/bin/doom sync -u &
     fi
+
     exec ${myemacs}/bin/emacs --daemon
   '';
 
