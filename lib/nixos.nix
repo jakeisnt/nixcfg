7f12a26e614b534a64c999c8e13aff06b2883657
Jacob Chvatal <jake@isnt.online>
2021-12-20 02:43:11 -0500

    restore nixos lib config

--- lib/nixos.nix
+++ lib/nixos.nix
@@ -12,24 +12,8 @@ in {
       modules = [
         inputs.sops-nix.nixosModules.sops
         {
-          imports = [
-            # basic installation framework structure
-            (pkgs.path + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-            # provides initial copy of nixos channel (?)
-            (pkgs.path + "/nixos/modules/installer/cd-dvd/channel.nix")
-          ];
           nixpkgs.pkgs = pkgs;
           networking.hostName = mkDefault hostname;
-          isoImage = {
-            volumeID = lib.mkForce hostname;
-            isoName = lib.mkForce "${hostname}.iso";
-            contents = [
-              {
-                source = pkgs.writeText "README" "NixOS installer from @jakeisnt's configuration!";
-                target = "/README.txt";
-              }
-            ];
-          };
         }
         (filterAttrs (n: v: !elem n [ "system" ]) attrs)
         ../.
