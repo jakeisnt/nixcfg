ea33a29a6abb02bd15868b63037c15d4ce2d559c
Jacob Chvatal <jake@isnt.online>
2022-01-06 03:21:34 -0500

    don't use latest nodePackages for js-ts

--- modules/dev/node.nix
+++ modules/dev/node.nix
@@ -17,7 +17,7 @@ in {
       yarn
 
       # :lang javascript
-      nodePackages_latest.typescript-language-server
+      nodePackages.typescript-language-server
     ];
 
     env.NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
