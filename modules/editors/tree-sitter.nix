2d49df772abba9225df8af1913c05ea95fae1811
Jacob Chvatal <jake@isnt.online>
2023-01-14 06:49:07 -0500

    add tree-sitter config file

--- modules/editors/tree-sitter.nix
+++ modules/editors/tree-sitter.nix
@@ -1,17 +1,17 @@
 { config, lib, pkgs, inputs, ... }:
 
-# Fetch and compile tree-sitter grammars ahead of time, rather than with a stateful configuration.
+# Fetch and compile tree-sitter grammars ahead of time rather than with a stateful configuration.
+# Inspired by https://github.com/Mic92/dotfiles.
 
 with lib;
 with lib.my;
 
 let
-    # from https://github.com/Mic92/dotfiles
     treeSitterGrammars = pkgs.runCommandLocal "grammars" {} ''
 mkdir -p $out/bin
 ${lib.concatStringsSep "\n"
     (lib.mapAttrsToList (name: src: "ln -s ${src}/parser $out/bin/${name}.so") pkgs.tree-sitter.builtGrammars)}
-'';
+        '';
     langs = [
         "bash"
         "c"
@@ -32,28 +32,22 @@ ${lib.concatStringsSep "\n"
         "org-nvim"
         "clojure"
         "commonlisp"
-        # "nu" # nushell: TODO
     ];
 
     grammars = lib.getAttrs (map (lang: "tree-sitter-${lang}") langs) pkgs.tree-sitter.builtGrammars;
     cfg = config.modules.editors.tree-sitter;
 
 in {
-
-
     options.modules.editors.tree-sitter = {
         enable = mkBoolOpt false;
     };
 
     config = mkIf cfg.enable {
-        user.packages = with pkgs; [
-            tree-sitter
-        ];
-
-        home.file.".tree-sitter".source = (pkgs.runCommand "grammars" {} ''
-        mkdir -p $out/bin
-        ${lib.concatStringsSep "\n"
-            (lib.mapAttrsToList (name: src: "name=${name}; ln -s ${src}/parser $out/bin/\${name#tree-sitter-}.so") grammars)}
-            '');
+        user.packages = with pkgs; [ tree-sitter ];
+        home.file.".tree-sitter".source = treeSitterGrammars;
+        home.configFile."tree-sitter" = {
+            source = "${configDir}/tree-sitter";
+            recursive = true; # include all utils, config files, etc...
         };
-    }
+    };
+}
