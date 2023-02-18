9b226e83304be195466538e0b1981e2ab37790f8
Jacob Chvatal <jake@isnt.online>
2023-02-17 19:39:07 -0500

    chrome improvin works

--- modules/browsers/chrome.nix
+++ modules/browsers/chrome.nix
@@ -7,6 +7,25 @@ in {
   options.modules.browsers.chrome = { enable = mkBoolOpt false; };
   config = mkIf cfg.enable {
     modules.browsers.chromium-help.enable = true;
-    user.packages = with pkgs; [ google-chrome-dev ];
+    # The profiles here correspond to profiles created dynamically when opening Chrome and signing in.
+    user.packages = with pkgs; [
+      google-chrome-dev
+      (makeDesktopItem {
+        name = "chrome-improvin";
+        desktopName = "Chrome (Improvin)";
+        genericName = "Open a Chrome window to the Improvin workspace";
+        icon = "google-chrome-unstable";
+        exec = "${google-chrome-dev}/bin/google-chrome-unstable --profile-directory=\"Profile 2\"";
+        categories = ["Network"];
+      })
+      (makeDesktopItem {
+        name = "chrome-default";
+        desktopName = "Chrome (Default)";
+        genericName = "Open a Chrome window to the default workspace";
+        icon = "google-chrome-unstable";
+        exec = "${google-chrome-dev}/bin/google-chrome-unstable --profile-directory=Default";
+        categories = ["Network"];
+      })
+    ];
   };
 }
