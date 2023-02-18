7fc76c51f87f392a5ab8ac17120377fd1cff115e
Jacob Chvatal <jake@isnt.online>
2023-02-17 20:22:55 -0500

    scripts to run chrome improvin

--- modules/browsers/chrome.nix
+++ modules/browsers/chrome.nix
@@ -3,19 +3,28 @@
 with lib;
 with lib.my;
 let cfg = config.modules.browsers.chrome;
+    chrome = pkgs.google-chrome-dev;
+    chrome-improvin = pkgs.writeShellScriptBin "chrome-improvin" ''
+      exec ${chrome}/bin/google-chrome-unstable --profile-directory="Profile 2" &
+    '';
+    chrome-default = pkgs.writeShellScriptBin "chrome-default" ''
+      exec ${chrome}/bin/google-chrome-unstable --profile-directory="Default" &
+    '';
 in {
   options.modules.browsers.chrome = { enable = mkBoolOpt false; };
   config = mkIf cfg.enable {
     modules.browsers.chromium-help.enable = true;
     # The profiles here correspond to profiles created dynamically when opening Chrome and signing in.
     user.packages = with pkgs; [
-      google-chrome-dev
+      chrome
+      chrome-improvin
+      chrome-default
       (makeDesktopItem {
         name = "chrome-improvin";
         desktopName = "Chrome (Improvin)";
         genericName = "Open a Chrome window to the Improvin workspace";
         icon = "google-chrome-unstable";
-        exec = "${google-chrome-dev}/bin/google-chrome-unstable --profile-directory=\"Profile 2\"";
+        exec = "${chrome-improvin}/bin/chrome-improvin";
         categories = ["Network"];
       })
       (makeDesktopItem {
@@ -23,7 +32,7 @@ in {
         desktopName = "Chrome (Default)";
         genericName = "Open a Chrome window to the default workspace";
         icon = "google-chrome-unstable";
-        exec = "${google-chrome-dev}/bin/google-chrome-unstable --profile-directory=Default";
+        exec = "${chrome-default}/bin/chrome-default";
         categories = ["Network"];
       })
     ];
