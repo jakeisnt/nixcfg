26dbe83c06ffdd50640afecaaffe0e24ac09be92
Jacob Chvatal <jake@isnt.online>
2024-04-18 22:29:02 +0200

    swap alacritty yml for toml

--- modules/term/alacritty.nix
+++ modules/term/alacritty.nix
@@ -13,56 +13,59 @@ in {
       "alacritty/alacritty.yml".text = with colors;
         concatStrings [
           (concatMapStringsSep "\n" readFile
-            [ "${configDir}/alacritty/alacritty.yml" ])
+            [ "${configDir}/alacritty/alacritty.toml" ])
           (''
-            colors:
-              primary:
-                background: '#${background}'
-                foreground: '#${foreground}'
-                dim_foreground: '#${fadeColor}'
-              cursor:
-                text: '#${background}'
-                cursor: '#${foreground}'
-              vi_mode_cursor:
-                text: '#${background}'
-                cursor: '#${foreground}'
-              selection:
-                text: CellForeground
-                background: '#${fadeColor}'
-              search:
-                matches:
-                  foreground: CellBackground
-                  background: '#${color6}'
-                # bar:
-                #   background: '#434c5e'
-                #   foreground: '#${foreground}'
-              normal:
-                black: '#${color0}'
-                red: '#${color1}'
-                green: '#${color2}'
-                yellow: '#${color3}'
-                blue: '#${color4}'
-                magenta: '#${color5}'
-                cyan: '#${color6}'
-                white: '#${color7}'
-              bright:
-                black: '#${color8}'
-                red: '#${color9}'
-                green: '#${color10}'
-                yellow: '#${color11}'
-                blue: '#${color12}'
-                magenta: '#${color13}'
-                cyan: '#${color14}'
-                white: '#${color15}'
-              dim:
-                black: '#${dim.black}'
-                red: '#${dim.red}'
-                green: '#${dim.green}'
-                yellow: '#${dim.yellow}'
-                blue: '#${dim.blue}'
-                magenta: '#${dim.magenta}'
-                cyan: '#${dim.cyan}'
-                white: '#${dim.white}'
+[colors]
+  [colors.primary]
+    background = '#${background}'
+    foreground = '#${foreground}'
+    dim_foreground = '#${fadeColor}'
+
+  [colors.cursor]
+    text = '#${background}'
+    cursor = '#${foreground}'
+
+  [colors.vi_mode_cursor]
+    text = '#${background}'
+    cursor = '#${foreground}'
+
+  [colors.selection]
+    text = "CellForeground"
+    background = '#${fadeColor}'
+
+  [colors.search.matches]
+    foreground = "CellBackground"
+    background = '#${color6}'
+
+  [colors.normal]
+    black = '#${color0}'
+    red = '#${color1}'
+    green = '#${color2}'
+    yellow = '#${color3}'
+    blue = '#${color4}'
+    magenta = '#${color5}'
+    cyan = '#${color6}'
+    white = '#${color7}'
+
+  [colors.bright]
+    black = '#${color8}'
+    red = '#${color9}'
+    green = '#${color10}'
+    yellow = '#${color11}'
+    blue = '#${color12}'
+    magenta = '#${color13}'
+    cyan = '#${color14}'
+    white = '#${color15}'
+
+  [colors.dim]
+    black = '#${dim.black}'
+    red = '#${dim.red}'
+    green = '#${dim.green}'
+    yellow = '#${dim.yellow}'
+    blue = '#${dim.blue}'
+    magenta = '#${dim.magenta}'
+    cyan = '#${dim.cyan}'
+      white = '#${dim.white}'
           '')
         ];
     };
