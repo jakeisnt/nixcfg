156e61efc9b7747116add39eeaacb332e9d6346a
Jacob Chvatal <jake@isnt.online>
2026-04-04 02:29:20 +0200

    remove chrome profile config, we only need one

--- modules/browsers/chrome.nix
+++ modules/browsers/chrome.nix
@@ -4,37 +4,11 @@ with lib;
 with lib.my;
 let cfg = config.modules.browsers.chrome;
     chrome = pkgs.google-chrome;
-    chrome-improvin = pkgs.writeShellScriptBin "chrome-improvin" ''
-      exec ${chrome}/bin/google-chrome-unstable --profile-directory="Profile 2" &
-    '';
-    chrome-default = pkgs.writeShellScriptBin "chrome-default" ''
-      exec ${chrome}/bin/google-chrome-unstable --profile-directory="Default" &
-    '';
 in {
   options.modules.browsers.chrome = { enable = mkBoolOpt false; };
   config = mkIf cfg.enable {
     modules.browsers.chromium-help.enable = true;
     # The profiles here correspond to profiles created dynamically when opening Chrome and signing in.
-    user.packages = with pkgs; [
-      chrome
-      chrome-improvin
-      chrome-default
-      (makeDesktopItem {
-        name = "chrome-improvin";
-        desktopName = "Chrome (Improvin)";
-        genericName = "Open a Chrome window to the Improvin workspace";
-        icon = "google-chrome-unstable";
-        exec = "${chrome-improvin}/bin/chrome-improvin";
-        categories = ["Network"];
-      })
-      (makeDesktopItem {
-        name = "chrome-default";
-        desktopName = "Chrome (Default)";
-        genericName = "Open a Chrome window to the default workspace";
-        icon = "google-chrome-unstable";
-        exec = "${chrome-default}/bin/chrome-default";
-        categories = ["Network"];
-      })
-    ];
+    user.packages = with pkgs; [ chrome ];
   };
 }
